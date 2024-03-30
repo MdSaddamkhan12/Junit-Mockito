ec6ea03558dbb1e8dd3f2f80b86c9d2c86cc0702
Saddam-Pc <saddam8101.sk@gmail.com>
2024-03-30 14:47:32 +0530

    added demoStubWithMockito method

--- src/test/java/com/saddam/mockito/test_double/stub/StubTest.java
+++ src/test/java/com/saddam/mockito/test_double/stub/StubTest.java
@@ -2,9 +2,13 @@ package com.saddam.mockito.test_double.stub;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class StubTest {
 
@@ -12,6 +16,26 @@ public class StubTest {
     public void demoStub(){
         BookRepository bookRepository = new BookRepositoryStub();
         BookService bookService = new BookService(bookRepository);
+
+        List<Book> newBooksWithAppliedDiscount = bookService.getNewBooksWithAppliedDiscount(10, 7);
+        assertEquals(2, newBooksWithAppliedDiscount.size());
+        assertEquals(450,newBooksWithAppliedDiscount.get(0).getPrice());
+        assertEquals(360,newBooksWithAppliedDiscount.get(1).getPrice());
+    }
+    @Test
+    public void demoStubWithMockito(){
+        BookRepository bookRepository = mock(BookRepository.class);
+        BookService bookService = new BookService(bookRepository);
+
+        List<Book> newBook = new ArrayList<>();
+
+        Book book1 = new Book("1234", "Mockito In Action", 500, LocalDate.now());
+        Book book2 = new Book("1235", "JUnit 5 In Action", 400, LocalDate.now());
+
+        newBook.add(book1);
+        newBook.add(book2);
+
+        when(bookRepository.findNewBook(7)).thenReturn(newBook);
 
         List<Book> newBooksWithAppliedDiscount = bookService.getNewBooksWithAppliedDiscount(10, 7);
         assertEquals(2, newBooksWithAppliedDiscount.size());
